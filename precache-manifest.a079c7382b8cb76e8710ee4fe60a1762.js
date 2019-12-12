1817a5ea868e5eb37d116e08a9b0ab87f901e04d
bojan.pilindavic <57361256+bojanpilindavic@users.noreply.github.com>
2019-12-12 14:55:23 +0100

    Updates

---
+++ precache-manifest.a079c7382b8cb76e8710ee4fe60a1762.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65bcf8ab87a8186029f581ddee7beeb9",
+    "url": "/changeMachine/index.html"
+  },
+  {
+    "revision": "bd81bb98c789fdcac2d7",
+    "url": "/changeMachine/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "4adb35487a1f8a4b4eb7",
+    "url": "/changeMachine/static/css/main.d75a1143.chunk.css"
+  },
+  {
+    "revision": "bd81bb98c789fdcac2d7",
+    "url": "/changeMachine/static/js/2.50dae14f.chunk.js"
+  },
+  {
+    "revision": "4adb35487a1f8a4b4eb7",
+    "url": "/changeMachine/static/js/main.72b3d9b9.chunk.js"
+  },
+  {
+    "revision": "8a971c0b48e31e9c79fb",
+    "url": "/changeMachine/static/js/runtime-main.6fc95324.js"
+  },
+  {
+    "revision": "8223cc58c5ef8ac37c8515f526e8bf5b",
+    "url": "/changeMachine/static/media/backround.8223cc58.jpg"
+  },
+  {
+    "revision": "338a71ad978b29f0706b95d9a6c668f0",
+    "url": "/changeMachine/static/media/backroundImage.338a71ad.jpeg"
+  },
+  {
+    "revision": "3f7e48bc2b2c382895aa2208de9602a9",
+    "url": "/changeMachine/static/media/slide2.3f7e48bc.png"
+  },
+  {
+    "revision": "9de9abe7b9645bce38d9c78c3cd8e986",
+    "url": "/changeMachine/static/media/slide3.9de9abe7.jpg"
+  }
+]);
